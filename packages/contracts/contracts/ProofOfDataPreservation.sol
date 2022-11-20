274ef6fb2a9c75fc7d98cd09967e14e1f260457b
taijusanagi <taijusanagi@gmail.com>
2022-11-20 17:05:17 +0900

    feat: poc of airdrop to miner

--- packages/contracts/contracts/ProofOfDataPreservation.sol
+++ packages/contracts/contracts/ProofOfDataPreservation.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity >=0.4.25 <=0.8.17;
+pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./CustomMarketAPI.sol";
+import "./CustomMinerAPI.sol";
+
+import "hardhat/console.sol";
 
 contract ProofOfDataPreservation is ERC721 {
   uint256 public totalSupply;
@@ -14,16 +18,79 @@ contract ProofOfDataPreservation is ERC721 {
 
   function claim(uint64 dealId) public {
     // get payload cid by deal id
-    // get miner by deal id
-    // MarketTypes.GetDealProviderParams memory getProviderParams = MarketTypes.GetDealProviderParams(dealId);
-    // MarketTypes.GetDealProviderReturn memory getProviderReturn = MarketAPI(marketAPI).get_deal_provider(
-    //   getProviderParams
-    // );
-    // string memory miner = getProviderReturn.provider;
-    // // bytes20(bytes(miner));
-    // // get miner evm address to airdrop
-    // address minerAddress = address(0x0); // this is the question
-    // // send token to the miner address
-    // _mint(minerAddress, totalSupply++);
+    // todo
+
+    // get provider by deal id
+    MarketTypes.GetDealProviderParams memory getProviderParams = MarketTypes.GetDealProviderParams(dealId);
+    MarketTypes.GetDealProviderReturn memory getProviderReturn = MarketAPI(marketAPI).get_deal_provider(
+      getProviderParams
+    );
+    address miner = convertStringAddressToAddress(getProviderReturn.provider);
+
+    // get send to address
+    // if beneficiary is registered, use beneficiary, if not, use owner
+    // NOTE: this is very experimental logic for hackathon, it should be updated according to the upcoming filecoin.sol
+    CustomMinerAPI minerAPI = CustomMinerAPI(miner);
+    address to;
+    try minerAPI.get_beneficiary() returns (MinerTypes.GetBeneficiaryReturn memory result) {
+      to = convertStringAddressToAddress(result.active.beneficiary);
+      // result.
+    } catch {
+      MinerTypes.GetOwnerReturn memory getOwnerReturn = minerAPI.get_owner();
+      to = convertStringAddressToAddress(getOwnerReturn.owner);
+    }
+    // send token to the miner address
+    _mint(to, totalSupply++);
+  }
+
+  // this method is not efficient, but it works properly to get the send to address from string data
+  function convertStringAddressToAddress(string memory stringAddress) public pure returns (address) {
+    require(bytes(stringAddress).length == 42, "ProofOfDataPreservation: address length is invalid");
+    require(
+      keccak256(abi.encodePacked(substring(stringAddress, 0, 2))) == keccak256(abi.encodePacked("0x")),
+      "ProofOfDataPreservation: address prefix is invalid"
+    );
+    return address(bytes20(fromHex(substring(stringAddress, 2, 42))));
+  }
+
+  /*
+   * https://ethereum.stackexchange.com/questions/39989/solidity-convert-hex-string-to-bytes
+   */
+  function fromHex(string memory s) public pure returns (bytes memory) {
+    bytes memory ss = bytes(s);
+    require(ss.length % 2 == 0); // length must be even
+    bytes memory r = new bytes(ss.length / 2);
+    for (uint i = 0; i < ss.length / 2; ++i) {
+      r[i] = bytes1(fromHexChar(uint8(ss[2 * i])) * 16 + fromHexChar(uint8(ss[2 * i + 1])));
+    }
+    return r;
+  }
+
+  /*
+   * https://ethereum.stackexchange.com/questions/39989/solidity-convert-hex-string-to-bytes
+   */
+  function fromHexChar(uint8 c) public pure returns (uint8) {
+    if (bytes1(c) >= bytes1("0") && bytes1(c) <= bytes1("9")) {
+      return c - uint8(bytes1("0"));
+    }
+    if (bytes1(c) >= bytes1("a") && bytes1(c) <= bytes1("f")) {
+      return 10 + c - uint8(bytes1("a"));
+    }
+    if (bytes1(c) >= bytes1("A") && bytes1(c) <= bytes1("F")) {
+      return 10 + c - uint8(bytes1("A"));
+    }
+    revert("fail");
+  }
+
+  /*
+   * https://ethereum.stackexchange.com/questions/31457/substring-in-solidity
+   */
+  function substring(string memory str, uint startIndex, uint endIndex) public pure returns (string memory) {
+    bytes memory strBytes = bytes(str);
+    bytes memory result = new bytes(endIndex - startIndex);
+    for (uint i = startIndex; i < endIndex; i++) {
+      result[i - startIndex] = strBytes[i];
+    }
+    return string(result);
   }
 }
--- packages/contracts/test/integration.test.ts
+++ packages/contracts/test/integration.test.ts
@@ -3,14 +3,19 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 import { CUSTOME_DEAL_ID } from "../config";
-import { CustomMarketAPI__factory, CustomMinerAPI__factory } from "../typechain-types";
+import {
+  CustomMarketAPI__factory,
+  CustomMinerAPI__factory,
+  ProofOfDataPreservation__factory,
+} from "../typechain-types";
 
 describe("integration", function () {
   async function fixture() {
-    const [signer, owner] = await ethers.getSigners();
+    const [signer, owner, beneficiary] = await ethers.getSigners();
     const customMinerAPI = await new CustomMinerAPI__factory(signer).deploy(owner.address);
     const customMarketAPI = await new CustomMarketAPI__factory(signer).deploy(CUSTOME_DEAL_ID, customMinerAPI.address);
-    return { signer, owner, customMinerAPI, customMarketAPI };
+    const proofOfDataPreservation = await new ProofOfDataPreservation__factory(signer).deploy(customMarketAPI.address);
+    return { signer, owner, beneficiary, customMinerAPI, customMarketAPI, proofOfDataPreservation };
   }
 
   it("make sure address mapping is correct", async () => {
@@ -19,5 +24,34 @@ describe("integration", function () {
     expect(getOwnerResult.owner).to.equal(owner.address);
     const getDealProviderResult = await customMarketAPI.get_deal_provider({ id: CUSTOME_DEAL_ID });
     expect(getDealProviderResult.provider).to.equal(customMinerAPI.address);
+  });
+
+  it("convertStringAddressToAddress", async () => {
+    const { signer, proofOfDataPreservation } = await fixture();
+    const result = await proofOfDataPreservation.convertStringAddressToAddress(
+      "0x29893eEFF38C5D5A1B2F693e2d918e618CCFfdD8"
+    );
+    expect(result).to.equal(signer.address);
+  });
+
+  it("claim to owner", async () => {
+    const { owner, proofOfDataPreservation } = await fixture();
+    const tokenId = await proofOfDataPreservation.totalSupply();
+    await expect(proofOfDataPreservation.claim(CUSTOME_DEAL_ID))
+      .to.emit(proofOfDataPreservation, "Transfer")
+      .withArgs(ethers.constants.AddressZero, owner.address, tokenId);
+  });
+
+  it("claim to beneficiary", async () => {
+    const { beneficiary, customMinerAPI, proofOfDataPreservation } = await fixture();
+    await customMinerAPI.change_beneficiary({
+      new_beneficiary: beneficiary.address,
+      new_quota: 0, // dummy
+      new_expiration: 0, // dummy
+    });
+    const tokenId = await proofOfDataPreservation.totalSupply();
+    await expect(proofOfDataPreservation.claim(CUSTOME_DEAL_ID))
+      .to.emit(proofOfDataPreservation, "Transfer")
+      .withArgs(ethers.constants.AddressZero, beneficiary.address, tokenId);
   });
 });
